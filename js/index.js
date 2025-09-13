fa514c96dd3c99b3d3809d2c29d3398132dc033b
Harris <Harrisimran55@gmail.com>
2025-09-13 20:41:14 +0500

    Code And Art Removed

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import "../scss/Main.scss";
+// import "./pre-loader";
+import "./side-bar";
+import "./elements-home";
+import "./marquee";
+import "./card";
+import "./faq-toggle";
+import "./ing-secction";
+import "./slider";
+import "./cart";
+import "./horizontal-text";
+
---
+++ js/product-page.js
@@ -0,0 +1,7 @@
+import "../scss/Main.scss";
+import "./side-bar";
+import "./marquee";
+import "./product-cmp";
+import "./cart";
+import "./ing-secction";
+import "./faq-toggle";
